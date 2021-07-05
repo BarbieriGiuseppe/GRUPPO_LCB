68131e4d6d1ba3b0543e050fe173ce0ed3e7c1d5
CardinaleChristian <cardinale.christian6@gmail.com>
2021-07-05 13:55:23 +0200

    #8 creato RegistrazioneController

--- testbooking/app/Http/Controllers/LoginController.php
+++ testbooking/app/Http/Controllers/LoginController.php
@@ -9,4 +9,20 @@ class LoginController extends Controller
     public function loginprivato(){
         return view('login.loginprivato');
     }
+
+    public function loginmedico(){
+        return view('login.loginlab');
+    }
+
+    public function loginlab(){
+        return view('login.loginlab');
+    }
+
+    public function loginazienda(){
+        return view('login.loginazienda');
+    }
+
+    public function loginasl(){
+        return view('login.loginasl');
+    }
 }
