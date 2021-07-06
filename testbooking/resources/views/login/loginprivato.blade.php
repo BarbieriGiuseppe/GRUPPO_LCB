6f9ca68177217405cae9384a7b83c3e3e0e08a5e
BarbieriGiuseppe <giuseppebarbieri16@gmail.com>
2021-07-06 15:04:41 +0200

    #8 aggiornamenti

--- testbooking/resources/views/login/loginprivato.blade.php
+++ testbooking/resources/views/login/loginprivato.blade.php
@@ -55,7 +55,7 @@
 					</div>
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button class="login100-form-btn" type="submit">
 							Login
 						</button>
 					</div>
