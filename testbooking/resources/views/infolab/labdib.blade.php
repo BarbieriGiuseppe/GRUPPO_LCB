75a7bd8b1a16c54b4e1147f6436bab0333c38580
BarbieriGiuseppe <giuseppebarbieri16@gmail.com>
2021-07-13 10:29:24 +0200

    #8 Aggiornamenti: Create le pagine di info dei laboratori

---
+++ testbooking/resources/views/infolab/labdib.blade.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <!-- Meta Tags -->
+  <meta charset="utf-8"/>
+
+  <!-- Site Title-->
+  <title>LaboratorioDib</title>
+
+  <!-- Mobile Specific Metas-->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+  <!-- Google-fonts -->
+  <link href='//fonts.googleapis.com/css?family=Signika+Negative:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link href='//fonts.googleapis.com/css?family=Kameron:400,700' rel='stylesheet' type='text/css'>
+  
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="<?php echo url('/infolaboratori'); ?>/css/bootstrap.min.css"/>
+  <!-- Fonts-style -->
+  <link rel="stylesheet" href="<?php echo url('/infolaboratori'); ?>/css/styles.css"/>
+  <!-- Fonts-style -->
+  <link rel="stylesheet" href="<?php echo url('/infolaboratori'); ?>/css/font-awesome.min.css"/>
+  <!-- Modal-Effect -->
+  <link href="<?php echo url('/infolaboratori'); ?>/css/component.css" rel="stylesheet">
+  <!-- owl-carousel -->
+  <link href="<?php echo url('/infolaboratori'); ?>/css/owl.carousel.css" rel="stylesheet" type="text/css" media="screen">
+  <link href="<?php echo url('/infolaboratori'); ?>/css/owl.theme.css" rel="stylesheet" type="text/css" media="screen">
+  <!-- Template Styles-->
+  <link rel="stylesheet" href="<?php echo url('/infolaboratori'); ?>/css/style.css"/>
+  <!-- Template Color-->
+  <link rel="stylesheet" type="text/css" href="<?php echo url('/infolaboratori'); ?>/css/green.css" media="screen" id="color-opt" />
+
+
+
+</head>
+
+<body data-spy="scroll" data-offset="80">
+
+  <!-- Preloader -->
+  <div class="animationload">
+    <div class="loader">
+        Attendere ....
+    </div>
+  </div> 
+  <!-- End Preloader -->
+
+
+  <nav class="navbar navbar-default navbar-fixed-top navbar-custom">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/">
+          <div class="logo-image">
+                <img src="<?php echo url('/'); ?>/img/logo.png" class="img-fluid">
+          </div>
+        
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+         
+     
+        </button>
+        <a class="navbar-brand" href="#">LaboratorioDib</a>
+        
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#services">Tipologie Tamponi</a></li>
+          <li><a href="#footer">Info</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+  </nav>
+
+    <!-- /HOME -->
+    <section class="main-home" id="home">
+      <div class="home-page-dib-photo"></div> <!-- Background image -->
+      <div class="home__header-content">
+        <div id="main-home-carousel" class="owl-carousel">
+          <div>
+            <h1 class="intro-title">Professionalità</h1>
+            <p class="intro-text">Cerchiamo di offrire un servizio che rispetti il benessere del paziente. <br/> Il paziente ha la nostra completa attenzione.</p>
+            
+          </div><!--slide item like paragraph-->
+
+          <div>
+            <h1 class="intro-title">Garanzia</h1>
+            <p class="intro-text">Ti garantiamo,ad un prezzo accessibile, un servizio affifabile. <br/> Garantiamo un'affidabilit� del tampone del 99%</p>
+          
+          </div><!--slide item like paragraph-->
+
+         
+          </div><!--slide item like paragraph-->
+        </div>
+      </div>
+    </section>
+    <!-- /End HOME -->
+
+    <!-- / SERVICES -->
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h3 class="title text-center">Tipologie Tamponi</h3>
+            <div class="titleHR"><span></span></div>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                </div>
+                <h5>Test salivari</h5>
+                <p>Il prelievo di saliva è più semplice e meno invasivo rispetto al tampone naso-faringeo o al prelievo di sangue <br/> Costo: 15€ <br>Tempistiche Esito: 2 Giorni</p></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                </div>
+                <h5>Test Molecolare</h5>
+                <p>Viene prelevato con un lungo bastoncino simile ad un cotton-fioc un campione delle vie respiratorie del paziente, preferibilmente dalla mucosa naso-faringea.<br> Costo: 50€<br>Tempistiche Esito: 4 Giorni</p></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4"> <!-- Service-item -->
+            <div class="text-center services-item">
+              <div class="col-wrapper">
+                <div class="icon-border"> 
+                </div>
+                <h5>Test Antigenico</h5>
+                <p>Questa tipologia di test è basata sulla ricerca, nei campioni respiratori del paziente, delle proteine virali (antigeni). Le modalità di raccolta del campione sono del tutto analoghe a quelle dei test molecolari (tampone naso-faringeo)<br> Costo:15€ <br> Tempistiche Esito:30 Minuti</p></p>
+              </div>
+            </div>
+          </div>
+        </div> <!--/.row -->
+
+        
+          
+            </div>
+          </div>
+        </div> <!--/.row -->
+      </div> <!--/.container -->
+    </section>
+    <!-- / End services-->
+
+
+
+    <!-- FOOTER begings -->
+    <footer id="footer">    
+      <div class="footer-content">
+        <div class="container text-center">    
+          <div class="row">
+            <div class="col-sm-4 col-md-4">
+              <div class="footer-content">
+                <h4>ORARIO</h4>
+                <div class="footer-orari">
+                  <p><b>Orari per effettuare il Tampone:</b><br>Lun - Ven --> 07:30 - 19:30 <br> Sab - Dom --> Chiuso<br> <b>Orari per prenotare il Tampone:</b><br>Lun - Ven --> 08:30 - 13:00 / 15:30 - 18:30 <br> Sab - Dom --> Chiuso </p>
+
+                </div>
+              </div> <!-- end footer-content -->
+            </div> <!-- end col-sm-4 -->
+            <div class="col-sm-4 col-md-4">
+              <div class="footer-content">
+                <h4>INDIRIZZO</h4>
+                <p>Via Edoardo Orabona, 4<br>
+                  70125 Bari BA</p>
+               
+              </div> <!-- end footer-content -->
+            </div> <!-- end col-sm-4 -->
+            <div class="col-sm-4 col-md-4">
+              <div class="footer-content">
+                <h4>CONTATTI</h4>
+                <p>080 544 3275<br>
+                  laboratoriodib@gmail.com</p>
+
+              </div>  <!-- end footer-content -->   
+            </div> <!-- end col-sm-4 -->
+          </div> <!-- end row -->
+        </div> <!-- container -->
+      </div>
+      <div class="footer-bottom text-center"> <!-- Footer-copyright -->
+        <p>�2021 GRUPPO_LCB <br> Realizzato da <a href="https://github.com/LorussoDanilo/GRUPPO_LCB">Gruppo LCB</a></p>
+      </div>
+    </footer>
+    <!-- End footer begings -->
+
+
+    <!-- Scroll top -->
+    <a href="#" class="back-to-top"> <i class="fa fa-chevron-up"> </i> </a>
+
+
+
+    <!-- JavaScript
+     ================================================== -->
+     <!-- Placed at the end of the document so the pages load faster -->
+     <!-- initialize jQuery Library -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/jquery.min.js"></script>
+     <!-- jquery easing -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/jquery.easing.min.js"></script>
+     <!-- Bootstrap -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/bootstrap.min.js"></script>
+     <!-- modal-effect -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/classie.js"></script>
+     <script src="<?php echo url('/infolaboratori'); ?>/js/modalEffects.js"></script>
+     <!-- Counter-up -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/waypoints.min.js" type="text/javascript"></script>
+     <script src="<?php echo url('/infolaboratori'); ?>/js/jquery.counterup.min.js" type="text/javascript"></script>
+     <!-- SmoothScroll -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/SmoothScroll.js"></script>
+     <!-- Parallax -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/jquery.stellar.min.js"></script>
+     <!-- Jquery-Nav -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/jquery.nav.js"></script>
+     <!-- Owl carousel -->                                                      
+     <script type="text/javascript" src="<?php echo url('/infolaboratori'); ?>/js/owl.carousel.min.js"></script>
+     <!-- App JS -->
+     <script src="<?php echo url('/infolaboratori'); ?>/js/app.js"></script>
+
+
+
+  </body>
+</html>
