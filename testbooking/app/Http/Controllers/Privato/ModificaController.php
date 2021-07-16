4dde06b2d0007cb438b031874bc02b144532fe38
CardinaleChristian <cardinale.christian6@gmail.com>
2021-07-16 18:42:05 +0200

    #8 Piccoli Aggiornamenti sulle Route

--- testbooking/app/Http/Controllers/Privato/ModificaController.php
+++ testbooking/app/Http/Controllers/Privato/ModificaController.php
@@ -14,48 +14,48 @@ use app\Http\Middleware\RedirectIfNotPrivato;
 
 class ModificaController extends Controller
 {
-    public  function mostraProfilo()
+   /* public  function mostraProfilo()
     {   
         $id = Auth::guard('privato')->user()->id-1;
         $data = DB::select('select * from privatos');               
         return view('privato/modifica',['data'=>$data,'id'=>$id]);
     }
+    */
 
-
-    public  function modificaAnagrafica($id)
+    public  function modificaAnagrafica()
     {   
         $id = Auth::guard('privato')->user()->id-1;
         $user = DB::select('select * from privatos');              
-        return view('privato/editanagrafica',['user'=>$user,'id'=>$id]);
+        return view('privato/modifica',['user'=>$user,'id'=>$id]);
     }
 
-    public  function updateAnagrafica($id)
+    public  function updateAnagrafica()
     {   
+        var_dump(10);
+        // $codicefiscale = $req->input('codicefiscale');
+        // $cognome = $req->input('cognome');
+        // $nome = $req->input('nome');
+        // $telefono = $req->input('telefono');
+        // $datanascita = $req->input('datanascita');
+        // $luogonascita = $req->input('luogonascita');
+        // $residenza = $req->input('residenza');
+        // $citta = $req->input('citta');
+        // $provincia = $req->input('provincia');
+        // $cap =$req->input('cap');
+        // $nazione = $req->input('nazione');
+        // $email = $req->input('email');
+        // $password =$req->input('password');
 
-        /*$codicefiscale = $req->input('codicefiscale');
-        $cognome = $req->input('cognome');
-        $nome = $req->input('nome');
-        $telefono = $req->input('telefono');
-        $datanascita = $req->input('datanascita');
-        $luogonascita = $req->input('luogonascita');
-        $residenza = $req->input('residenza');
-        $citta = $req->input('citta');
-        $provincia = $req->input('provincia');
-        $cap =$req->input('cap');
-        $nazione = $req->input('nazione');
-        $email = $req->input('email');
-        $password =$req->input('password');
 
-
-        DB::update('update privatos set codicefiscale = ? ,
-        cognome = ?, nome = ? ,telefono = ? ,datanascita = ? ,
-        luogonascita = ? ,residenza = ? ,citta = ? ,provincia = ? ,
-        cap = ? ,nazione = ? ,email = ? ,password = ?  where id = ?',
-        [ $codicefiscale ,$cognome , $nome ,$telefono ,$datanascita ,$luogonascita ,
-        $residenza ,$citta ,$provincia ,$cap ,$nazione ,$email  ,$password ,$id]);*/
+        // DB::update('update privatos set codicefiscale = ? ,
+        // cognome = ?, nome = ? ,telefono = ? ,datanascita = ? ,
+        // luogonascita = ? ,residenza = ? ,citta = ? ,provincia = ? ,
+        // cap = ? ,nazione = ? ,email = ? ,password = ?  where id = ?',
+        // [ $codicefiscale ,$cognome , $nome ,$telefono ,$datanascita ,$luogonascita ,
+        // $residenza ,$citta ,$provincia ,$cap ,$nazione ,$email  ,$password ,$id]);*/
 
         
-        return redirect('privato/modifica');
+        // return redirect('privato/modifica');
     }
   
 }
